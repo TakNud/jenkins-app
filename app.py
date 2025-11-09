b6df7b84ed982bba32e7625858165a98dd76a6ee
ASofer72 <ASofer72@horizon285.com>
2025-11-09 19:48:04 +0200

    init flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Hello from Flask in Docker 🚀</h1><p>This is a simple page.</p>"
+
+if __name__ == "__main__":
+    # host=0.0.0.0 so it's accessible from Docker/container
+    app.run(host="0.0.0.0", port=5000, debug=True)
