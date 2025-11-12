9705d32ab0007f0cec306adb243189be279dc16a
ASofer72 <ASofer72@horizon285.com>
2025-11-12 16:52:13 +0200

    testSCM1111

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return "<h1>Hello from Flask in Docker 🚀</h1><p>This is a simple page.</p>"
 
 if __name__ == "__main__":
+    # host=0.0.0.0 so it's accessible from Docker/container
     app.run(host="0.0.0.0", port=5000, debug=True)
