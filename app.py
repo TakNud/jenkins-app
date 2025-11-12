068acae79dbb429959f8ea69d0962b0e1edbdb69
ASofer72 <ASofer72@horizon285.com>
2025-11-12 16:55:09 +0200

    testSS

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def home():
     return "<h1>TESTESTEST 🚀</h1><p>This is a simple page.</p>"
 
+@app.route("/test2")
+def home():
+    return "<h1>TEST2EST2EST2 🚀</h1><p>This is a simple page.</p>"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
